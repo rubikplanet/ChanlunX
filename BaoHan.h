84767208d4d17217191aa4754a3579218e4f0f92
kldcty <1106628276@qq.com>
2018-12-21 20:13:34 +0800

    clean code

--- BaoHan.h
+++ BaoHan.h
@@ -6,7 +6,8 @@
 // 处理Ｋ线的包含关系
 // 输入是K线高（*pHigh）和K线低（*pLow）
 // 输出是K线的方向（*pDirection）、处理后的Ｋ线高（*pOutHigh）、处理后的K线低（*pOutLow）、K线是否有包含（*pInclude）
-void BaoHan(int nCount, float *pDirection, float *pOutHigh, float *pOutLow, float *pInclude, float *pHigh, float *pLow);
+void BaoHan(int nCount, float *pDirection, float *pOutHigh, float *pOutLow,
+		float *pInclude, float *pHigh, float *pLow);
 
 #pragma pack(pop)
 
