67a7ac9d0632954a8bb8d2eb27ec25fc73a9c5e8
caojing20201203 <75433118+caojing20201203@users.noreply.github.com>
2021-08-14 21:33:37 +0800

    重新编写Kxian

--- BaoHanChuLi.cpp
+++ BaoHanChuLi.cpp
@@ -2,74 +2,50 @@
 
 using namespace std;
 
-int BaoHanChuLi::count = 0;
-
 Kxian1 BaoHanChuLi::add(float gao, float di)
 {
-    KxianRaw1 kxianRaw;
-    kxianRaw.gao = gao;
-    kxianRaw.di = di;
-    this->kxianRawList.push_back(kxianRaw);
+    Kxian1 kxian;
 
     if (this->kxianList.empty()) {
         //假设第一根K线方向向上
-        Kxian1 kxian;
-        
         kxian.gao = gao;
         kxian.di = di;
-        kxian.fangxiang = 1;
-        kxian.status = true;
+        kxian.fangxiang = Kxian_UP;
         kxian.position = BaoHanChuLi::count;
-        this->kxianList.push_back(kxian);
-        BaoHanChuLi::count += 1;
-        return(kxian);
     }
     else {
         if (gao > this->kxianList.back().gao && di > this->kxianList.back().di) {
             //向上
-            Kxian1 kxian;
-
             kxian.gao = gao;
             kxian.di = di;
-            kxian.fangxiang = 1;
-            kxian.status = true;
+            kxian.fangxiang = Kxian_UP;
             kxian.position = BaoHanChuLi::count;
-            this->kxianList.push_back(kxian);
-            BaoHanChuLi::count += 1;
-            return(kxian);
-        } else {
+        }
+        else {
             if (gao < this->kxianList.back().gao && di < this->kxianList.back().di) {
                 //向下
-                Kxian1 kxian;
-
                 kxian.gao = gao;
                 kxian.di = di;
-                kxian.fangxiang = -1;
-                kxian.status = true;
+                kxian.fangxiang = Kxian_DOWN;
                 kxian.position = BaoHanChuLi::count;
-                this->kxianList.push_back(kxian);
-                BaoHanChuLi::count += 1;
-                return(kxian);
-            } else{
-                Kxian1 kxian;
-
+            }
+            else {
                 kxian = this->kxianList.back();
-                if (gao <= kxian.gao && di >= kxian.di){
-                    //前包含
-                    if (kxian.fangxiang == 1) {
+                this->kxianList.pop_back();
+                if (gao <= kxian.gao && di >= kxian.di) {
+                    //1包含2
+                    if (kxian.fangxiang == Kxian_UP)
                         kxian.di = di;
-                    } else {
+                    else
                         kxian.gao = gao;
-                    }
-                } else {
-                    //后包含
-                    if (kxian.fangxiang == 1) {
-                        kxian.gao = gao;
-                    } else {
-                        kxian.di = di;
-                    }
                 }
-                kxian.status = false;
+                else {
+                    //2包含1
+                    if (kxian.fangxiang == Kxian_UP)
+                        kxian.gao = gao;
+                    else
+                        kxian.di = di;
+                }
                 kxian.position = BaoHanChuLi::count;
                 this->kxianList.push_back(kxian);
                 BaoHanChuLi::count += 1;
@@ -77,4 +53,8 @@ Kxian1 BaoHanChuLi::add(float gao, float di)
             }
         }
     }
+
+    this->kxianList.push_back(kxian);
+    BaoHanChuLi::count += 1;
+    return(kxian);
 }
--- BaoHanChuLi.h
+++ BaoHanChuLi.h
@@ -7,31 +7,26 @@ using namespace std;
 
 #pragma pack(push, 1)
 
-//原始K线
-struct KxianRaw1
-{
-    float gao;
-    float di;
-};
-
 //包含处理后的K线
+
+//K线方向
+#define Kxian_UP        1   
+#define Kxian_DOWN      -1
+
 struct Kxian1
 {
     float gao;
     float di;
     int fangxiang;
-    bool status;
     int position; /*从起始K线开始的位置 */
-    int fx_type; /* 1-顶分型, -1-底分型, 0-普通 */
 };
 
 class BaoHanChuLi
 {
     public:
-      vector<KxianRaw1> kxianRawList;
       vector<Kxian1> kxianList;
       Kxian1 add(float gao, float di);
-      static int count;
+      int count = 0;
 };
 
 #pragma pack(pop)
